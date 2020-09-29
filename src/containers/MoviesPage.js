afc9b18c18e29907ea37a6a2ab3117af12d21091
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-29 05:58:10 +0000

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,26 @@
 // .src/containers/MoviesPage.js
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
+import MovieShow from './MovieShow';
+import MoviesNew from './MoviesNew';
 
-const MoviesPage = ({ match, movies }) => (
+/*const MoviesPage = ({ match, movies }) => (
+  <div> */
+  const MoviesPage = ({ match, movies }) =>
   <div>
+  <Switch>
     <MoviesList movies={movies} />
-  </div>
+  //</div>
+  <Route path={`${match.url}/new`} component={MoviesNew} />
+   <Route path={`${match.url}/:movieId`} component={MovieShow}/>
+   <Route exact path={match.url} render={() => (
+     <h3>Please select a Movie from the list.</h3>
+   )}/>
+ </Switch>
+</div>;
 )
 
 export default MoviesPage
